ed3b51ed5f08e18447d3151aa58790e9ce4cafd5
Halember1999 <mounikahalember@gmail.com>
2021-12-07 11:57:36 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// var coll=document.getElementsByClassName("collapse");
+// coll.addEventListener("click",function(){
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if(content.style.display === "block"){
+//         content.style.display="none";
+//     }
+//     else{
+//         content.style.display="block";
+//     }
+// });
+// $('.collapse').collapse()
